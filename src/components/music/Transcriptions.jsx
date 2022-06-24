f3be71f6cdaa043e94c03e7f2a3b7281435bbca1
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:14:42 -0600

    added Transcriptions component. This is a placeholder to later be built out

---
+++ src/components/music/Transcriptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Transcriptions() {
+	return (
+		<React.Fragment>
+			<h2>Transcriptions</h2>
+			<div>List of transcriptions</div>
+			</React.Fragment>
+	)
+}
+
+export default Transcriptions
