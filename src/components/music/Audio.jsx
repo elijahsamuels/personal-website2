2602d8665d9409929aa18db423029f19338eb5f2
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:14:02 -0600

    added Audio component

---
+++ src/components/music/Audio.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Audio() {
+	return (
+		<React.Fragment>
+			<h2>Audio</h2>
+			<div>List of Audio</div>
+			<ul>
+				<li>Audio Player</li>
+				<li>Audio Player</li>
+				<li>Audio Player</li>
+				<li>Audio Player</li>
+			</ul>
+			</React.Fragment>
+	)
+}
+
+export default Audio
