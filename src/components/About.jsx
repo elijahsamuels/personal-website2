99627879324daf304bb1d271fa989141c2735c20
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:10:48 -0600

    added about

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Timeline from "./Timeline.jsx";
+
+const About = () => {
+  return (
+    <div className="page">
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis a ipsa
+        distinctio quos libero commodi cum assumenda dicta inventore, enim
+        nesciunt doloremque sit deserunt! Aut dicta blanditiis saepe
+        necessitatibus illo? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Officiis a ipsa distinctio quos libero commodi cum assumenda dicta
+        inventore, enim nesciunt doloremque sit deserunt! Aut dicta blanditiis
+        saepe necessitatibus illo? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Officiis a ipsa distinctio quos libero commodi cum
+        assumenda dicta inventore, enim nesciunt doloremque sit deserunt! Aut
+        dicta blanditiis saepe necessitatibus illo? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Officiis a ipsa distinctio quos libero
+        commodi cum assumenda dicta inventore, enim nesciunt doloremque sit
+        deserunt! Aut dicta blanditiis saepe necessitatibus illo?
+      </p>
+      <div>
+        <Timeline />
+      </div>
+    </div>
+  );
+};
+
+export default About;
