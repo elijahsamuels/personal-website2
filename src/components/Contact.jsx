c3d3e11fe3525437f28ac23a28f03ce31760b785
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:11:25 -0600

    added contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContactForm from "./ContactForm";
+
+function Contact() {
+  return (
+    <React.Fragment>
+      <h2>Contact</h2>
+      <ContactForm />
+    </React.Fragment>
+  );
+}
+
+export default Contact;
