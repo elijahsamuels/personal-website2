8b53880fb1d26f7b365d326380601b61163d15c5
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:14:17 -0600

    added Calendar component

---
+++ src/components/music/Calendar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GoogleCalendar from "../../staticComponents/GoogleCalendar"
+
+function Calendar() {
+  return (
+    <React.Fragment>
+      <h1>Calendar</h1>
+			<GoogleCalendar/>
+    </React.Fragment>
+  );
+}
+
+export default Calendar;
