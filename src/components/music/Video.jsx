e5bb13821fa44603547190d4c701f507e232b5cb
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:18:51 -0600

    Video component. This is a placeholder to be built out.

---
+++ src/components/music/Video.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Video() {
+	return (
+		<React.Fragment>
+			<h2>Video</h2>
+			<div>List of Video</div>
+			<ul>
+				<li>Video Player</li>
+				<li>Video Player</li>
+				<li>Video Player</li>
+				<li>Video Player</li>
+			</ul>
+
+			</React.Fragment>
+	)
+}
+
+export default Video
