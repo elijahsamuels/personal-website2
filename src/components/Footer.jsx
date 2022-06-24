7e96759baa4fd15503f631cbb938ad03e9f39972
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:12:07 -0600

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { SiFacebook, SiLinkedin, SiGithub } from "react-icons/si";
+import { VscMail } from "react-icons/vsc";
+
+const Footer = () => {
+  return (
+    <div>
+      <a
+        href="https://www.linkedin.com/in/elijahsamuels"
+        target="_blank"
+        rel="noreferrer"
+        className="link social-media-icon"
+      >
+        <SiLinkedin size="1em" />
+      </a>
+      <a
+        href="https://github.com/elijahsamuels"
+        target="_blank"
+        rel="noreferrer"
+        className="link social-media-icon"
+      >
+        <SiGithub size="1em" />
+      </a>
+      <a
+        href="https://www.facebook.com/elijah.samuels.1"
+        target="_blank"
+        rel="noreferrer"
+        className="link social-media-icon"
+      >
+        <SiFacebook size="1em" />
+      </a>
+      <a
+        href="mailto:elijahsamuels@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+        className="link social-media-icon"
+      >
+        <VscMail size="1em" />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
