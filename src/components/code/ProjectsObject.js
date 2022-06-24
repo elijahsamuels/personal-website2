15b4a1442be3265ad0872d5771c2a07cefa6df52
elijahsamuels <elijahsamuels@gmail.com>
2022-06-24 11:15:47 -0600

    added ProjectsObject.js

---
+++ src/components/code/ProjectsObject.js
@@ -0,0 +1,53 @@
+// name
+// description
+// skills_used
+
+const Projects = [
+  {
+      name: "Wordle-clone",
+      description: "sample filler text",
+      skills_used: ["JavaScript", "CSS", "HTML", "Netlify", "Github"],
+			prod_link: "https://dashboard.heroku.com",
+			github_link: "https://github.com/elijahsamuels/wordle-clone"
+  },
+  {
+      name: "Dru Heller Website",
+      description: "sample filler text",
+      skills_used: ["Gatsby", "JavaScript", "CSS", "HTML", "Netlify", "Github"],
+			prod_link: "http://www.druhellermusic.com",
+			github_link: "https://github.com/elijahsamuels/druheller"
+  },
+  {
+      name: "QRCode",
+      description: "sample filler text",
+      skills_used: [
+        "React",
+        "React-QRcode",
+        "JavaScript",
+        "CSS",
+        "HTML",
+        "Netlify",
+        "Github",
+      ],
+			prod_link: "https://free-qrcode.netlify.app",
+			github_link: "https://github.com/elijahsamuels/qrcode"
+  },
+  {
+      name: "ElijahSamuels.com",
+      description: "sample filler text",
+      skills_used: [
+        "React",
+        "JavaScript",
+        "CSS",
+        "HTML",
+        "Netlify",
+        "Github",
+      ],
+			prod_link: "https://www.elijahsamuels.com",
+			github_link: "https://github.com/elijahsamuels/qrcode"
+  },
+];
+
+export default Projects;
+
+// 
