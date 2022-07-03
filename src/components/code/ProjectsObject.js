6bcbc49775cf14a756660e4e26fe7f846790c1f2
elijahsamuels <elijahsamuels@gmail.com>
2022-07-03 14:00:49 -0600

    changed link prod link for projects

--- src/components/code/ProjectsObject.js
+++ src/components/code/ProjectsObject.js
@@ -1,53 +1,40 @@
-// name
-// description
-// skills_used
-
 const Projects = [
   {
-      name: "Wordle-clone",
-      description: "A clone of the popular word game, Wordle!",
-      skills_used: ["JavaScript", "CSS", "HTML", "Heroku", "Github"],
-			prod_link: "https://dashboard.heroku.com",
-			github_link: "https://github.com/elijahsamuels/wordle-clone"
+    name: "Wordle-clone",
+    description: "A clone of the popular word game, Wordle!",
+    skills_used: ["JavaScript", "Node", "CSS", "HTML", "Heroku", "Github"],
+    prod_link: "https://es-wordle.herokuapp.com/",
+    github_link: "https://github.com/elijahsamuels/wordle-clone",
   },
   {
-      name: "Dru Heller Website",
-      description: "A site built for a great musician and educator, Dru Heller.",
-      skills_used: ["Gatsby", "JavaScript", "CSS", "HTML", "Netlify", "Github"],
-			prod_link: "http://www.druhellermusic.com",
-			github_link: "https://github.com/elijahsamuels/druheller"
+    name: "Dru Heller Website",
+    description: "A site built for a great musician and educator, Dru Heller.",
+    skills_used: ["Gatsby", "JavaScript", "CSS", "HTML", "Netlify", "Github"],
+    prod_link: "http://www.druhellermusic.com",
+    github_link: "https://github.com/elijahsamuels/druheller",
   },
   {
-      name: "QRCode",
-      description: "Get your free QR code here!",
-      skills_used: [
-        "React",
-        "React-QRcode",
-        "JavaScript",
-        "CSS",
-        "HTML",
-        "Netlify",
-        "Github",
-      ],
-			prod_link: "https://free-qrcode.netlify.app",
-			github_link: "https://github.com/elijahsamuels/qrcode"
+    name: "QRCode",
+    description: "Get your free QR code here!",
+    skills_used: [
+      "React",
+      "React-QRcode",
+      "JavaScript",
+      "CSS",
+      "HTML",
+      "Netlify",
+      "Github",
+    ],
+    prod_link: "https://free-qrcode.netlify.app",
+    github_link: "https://github.com/elijahsamuels/qrcode",
   },
   {
-      name: "ElijahSamuels.com",
-      description: "Personal website project",
-      skills_used: [
-        "React",
-        "JavaScript",
-        "CSS",
-        "HTML",
-        "Netlify",
-        "Github",
-      ],
-			prod_link: "https://www.elijahsamuels.com",
-			github_link: "https://github.com/elijahsamuels/qrcode"
+    name: "ElijahSamuels.com",
+    description: "Personal website project",
+    skills_used: ["React", "JavaScript", "CSS", "HTML", "Netlify", "Github"],
+    prod_link: "https://www.elijahsamuels.com",
+    github_link: "https://github.com/elijahsamuels/qrcode",
   },
 ];
 
 export default Projects;
-
-// 
