c3cfa23b2ec1b60245533c52cf454015cc51772d
elijahsamuels <elijahsamuels@gmail.com>
2022-07-03 12:38:00 -0600

    added Index.jsx file

---
+++ src/components/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import ColorChanger from "../staticComponents/ColorChanger";
+import Greeting from "../staticComponents/Greeting";
+
+function Index() {
+  return (
+    <div>
+      {/* <div>Index</div> */}
+			<Greeting />
+    </div>
+  );
+}
+
+export default Index;
