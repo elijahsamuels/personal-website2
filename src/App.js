40a6e88332452e3d807f5191e17d187bc2ee9210
elijahsamuels <elijahsamuels@gmail.com>
2023-07-28 20:05:37 -0600

    testing update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Index from "./components/Index";
 import Music from "./components/Music";
-import Calendar from "./components/Music/Calendar";
+// import Calendar from "./components/Music/Calendar";
 import Transcriptions from "./components/Music/Transcriptions";
 import Audio from "./components/Music/Audio";
 import Video from "./components/Music/Video";
-// import Code from "./components/Code";
-// import Projects from "./components/Code/Projects";
-// import Skills from "./components/Code/Skills";
+import Code from "./components/Code";
+import Projects from "./components/Code/Projects";
+import Skills from "./components/Code/Skills";
 import Footer from "./components/Footer";
 import Navbar from "./components/NavBar";
 // import { Navbar2 as Navbar } from "./components/Navbar2";
@@ -28,15 +28,15 @@ const App = () => {
           <Route path="/" element={<Index />} />
           <Route path="about" element={<About />} />
           <Route path="music" element={<Music />}>
-            <Route path="calendar" element={<Calendar />} />
+            {/* <Route path="calendar" element={<Calendar />} /> */}
             <Route path="transcriptions" element={<Transcriptions />} />
             <Route path="audio" element={<Audio />} />
             <Route path="video" element={<Video />} />
           </Route>
-          {/* <Route path="code" element={<Code />}>
+          <Route path="code" element={<Code />}>
             <Route path="projects" element={<Projects />} />
             <Route path="skills" element={<Skills />} />
-          </Route> */}
+          </Route>
           <Route path="contact" element={<Contact />} />
         </Routes>
       </div>
